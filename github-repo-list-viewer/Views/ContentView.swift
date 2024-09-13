69b3ae0e1dc6e221f88bd26c5d86f8161af98f28
Betül Çalık <80328286+betulcalik@users.noreply.github.com>
2024-09-14 00:13:48 +0300

    created project structure

---
+++ github-repo-list-viewer/Views/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  github-repo-list-viewer
+//
+//  Created by Betül Çalık on 13.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
